c6cc87d44bc2f2d281f9c5da39002be066ef6faf
lukemartinlogan <lukemartinlogan@gmail.com>
2023-06-05 04:57:17 -0500

    Make ticket test smaller

--- hermes_shm/test/unit/data_structures/containers/ticket_queue.cc
+++ hermes_shm/test/unit/data_structures/containers/ticket_queue.cc
@@ -31,7 +31,7 @@ TEST_CASE("TestTicketQueueIntMultiThreaded") {
   Allocator *alloc = alloc_g;
   REQUIRE(alloc->GetCurrentlyAllocatedSize() == 0);
   ProduceAndConsume<hipc::ticket_queue<int>, int>(8, 1, 8192, 64);
-  ProduceAndConsume<hipc::ticket_queue<int>, int>(8, 8, 100000, 64);
+  ProduceAndConsume<hipc::ticket_queue<int>, int>(8, 8, 8192, 64);
   REQUIRE(alloc->GetCurrentlyAllocatedSize() == 0);
 }
 
@@ -46,6 +46,6 @@ TEST_CASE("TestSplitTicketQueueIntMultiThreaded") {
   Allocator *alloc = alloc_g;
   REQUIRE(alloc->GetCurrentlyAllocatedSize() == 0);
   ProduceAndConsume<hipc::split_ticket_queue<int>, int>(8, 1, 8192, 64);
-  ProduceAndConsume<hipc::split_ticket_queue<int>, int>(8, 8, 100000, 64);
+  ProduceAndConsume<hipc::split_ticket_queue<int>, int>(8, 8, 8192, 64);
   REQUIRE(alloc->GetCurrentlyAllocatedSize() == 0);
 }
