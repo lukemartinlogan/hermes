b08f8010b564a8385c8c9fe1550e6d854162e405
lukemartinlogan <lukemartinlogan@gmail.com>
2023-10-10 05:37:21 -0500

    Reduce ops

--- benchmark/test_latency.cc
+++ benchmark/test_latency.cc
@@ -276,7 +276,7 @@ TEST_CASE("TestRoundTripLatency") {
   ProcessAffiner::SetCpuAffinity(pid, 10);
 
   t.Resume();
-  size_t ops = (1 << 15);
+  size_t ops = (1 << 8);
   // size_t ops = 1024;
   for (size_t i = 0; i < ops; ++i) {
     // client.MdPushRoot(labstor::DomainId::GetLocal());
